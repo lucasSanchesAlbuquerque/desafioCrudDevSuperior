2b8af105328dbb182d9347091102b841432a3048
Lucas SA <lusanchesx@gmail.com>
2025-01-23 11:57:50 -0300

    Criando projeto e configurando pom e properties

---
+++ src/main/java/com/desafioCrudDevSuperior/desafioCrudDevSuperior/DesafioCrudDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.desafioCrudDevSuperior.desafioCrudDevSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudDevSuperiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioCrudDevSuperior/desafioCrudDevSuperior/DesafioCrudDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioCrudDevSuperior.desafioCrudDevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
